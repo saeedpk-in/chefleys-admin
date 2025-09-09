20df02b21753fa595364b8fd4bf54f9b8e7f9d99
saeed pk <saeedpk781@gmail.com>
2025-09-09 23:48:41 +0530

    Second Commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,23 @@
-import { LoginForm } from "@/components/forms/LoginForm"
-
+import { LoginForm } from "@/components/forms/LoginForm";
+import Spinner from "@/components/Spinner";
+import { Suspense } from "react";
 
 const page = () => {
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
-        <LoginForm />
+        <Suspense
+          fallback={
+            <div className="w-full h-full justify-center items-center">
+              <Spinner />
+            </div>
+          }
+        >
+          <LoginForm />
+        </Suspense>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
